ffb1b183afb23a981ece68ba7dda8fe97a942dc8
Scott Schreiber <citrusspoon@gmail.com>
2017-11-08 00:20:49 +0000

    Added test drinks

--- shakeBake/db/migrate/20171108000526_create_drinks.rb
+++ shakeBake/db/migrate/20171108000526_create_drinks.rb
@@ -2,7 +2,7 @@ class CreateDrinks < ActiveRecord::Migration[5.1]
   def change
     create_table :drinks do |t|
       t.string :name
-      t.text :desc
+      t.text :desc 
 
       t.timestamps
     end
