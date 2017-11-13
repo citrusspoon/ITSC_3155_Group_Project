b32c5fd6d8f16d7d2039eca81351c766ca02d2db
Scott Schreiber <citrusspoon@gmail.com>
2017-11-13 21:41:08 +0000

    Ran migration after adding bread model

--- shakeBake/db/schema.rb
+++ shakeBake/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113213727) do
+ActiveRecord::Schema.define(version: 20171113214001) do
+
+  create_table "breads", force: :cascade do |t|
+    t.string "name"
+    t.text "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "drinks", force: :cascade do |t|
     t.string "name"
