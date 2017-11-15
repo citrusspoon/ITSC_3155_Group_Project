b1970a0070915171b683f21387747e58bde9e5fd
DavidnCothron <davidncothron@gmail.com>
2017-11-15 21:42:12 +0000

    Added pair model with references to drink and bread

--- shakeBake/config/routes.rb
+++ shakeBake/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get 'site_info/home'
+  resources :pairing_controller
   root to: 'site_info#home'
 
 end
