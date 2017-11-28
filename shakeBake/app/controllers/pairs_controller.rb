13054e1a4cd59fc6c7901d3adf3f4f5af14ce367
Scott Schreiber <citrusspoon@gmail.com>
2017-11-28 23:58:10 +0000

    added code to pairs controller

--- shakeBake/app/controllers/pairs_controller.rb
+++ shakeBake/app/controllers/pairs_controller.rb
@@ -1,2 +1,10 @@
 class PairsController < ApplicationController
+    def create
+        @drink = Drink.find(params[:drink_id])
+        @bread = Bread.find(params[:bread_id])
+        @pair = @drink.pairs.create
+        
+    end
+        
+            
 end
