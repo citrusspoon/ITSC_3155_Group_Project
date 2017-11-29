72f6fe411e1925138f0777e28575cd72914f1451
Scott Schreiber <citrusspoon@gmail.com>
2017-11-29 22:06:22 +0000

    added query code

--- shakeBake/app/controllers/pairing_controller.rb
+++ shakeBake/app/controllers/pairing_controller.rb
@@ -1,12 +1,19 @@
 class PairingController < ApplicationController
   def new
+    Pair.new
   end
   
   def show
+    @pair = Pair.find(params[:id])
   end
   
   def query
-    Pair.find_by_sql("SELECT * FROM pairs WHERE #{params[:d_option]} == drink AND #{params[:b_option]} == bread")
+    @query = Pair.find_by_sql("SELECT * FROM pairs WHERE #{params[:d_option]} == drink AND #{params[:b_option]} == bread;")
+    if @query.blank?
+      redirect_to :action => "new"
+    else
+      redirect_to :action => "show", :id => @query[0]['id'].class.inspect.to_i
+    end
   end
   
   def index
